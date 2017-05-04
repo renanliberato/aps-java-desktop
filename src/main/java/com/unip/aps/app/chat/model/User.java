21374b5b7c6835f904a765bd9e37a0d79343540f
Renan Liberato <renan.libsantana@gmail.com>
2017-05-03 21:55:44 -0300

    User model
    User model implementation, to persist the username statically across the app

---
+++ src/main/java/com/unip/aps/app/chat/model/User.java
@@ -0,0 +1,22 @@
+package com.unip.aps.app.chat.model;
+
+/**
+ * Created on 3/5/2017.
+ *
+ * @author Renan Liberato Sant'Ana <renan.libsantana@gmail.com>
+ */
+public class User {
+
+    /**
+     * Username do usuário que deve ficar disponível para uso pelo recurso de chat.
+     */
+    private static String username;
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void setUsername(String username) {
+        User.username = username;
+    }
+}
